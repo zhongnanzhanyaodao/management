0b365d80b1916a18386e595c3b25add46849c2f6
zhongnanzhanyaodao <262548405@qq.com>
2018-04-12 15:42:56 +0800

    kn

--- src/test/java/knowledge/thread/SemaphoreExample.java
+++ src/test/java/knowledge/thread/SemaphoreExample.java
@@ -3,26 +3,26 @@ package knowledge.thread;
 import java.util.concurrent.Semaphore;
 
 public class SemaphoreExample {
-    private static Semaphore semaphoreA = new Semaphore(1);
+    private static Semaphore semaphore = new Semaphore(1);
 
     public static void main(String[] args) throws InterruptedException {
         new Thread(() -> {
             try {
                 Thread.sleep(5*1000);
-                semaphoreA.release();
-                System.out.println("子线程："+semaphoreA.availablePermits());
+                semaphore.release();
+                System.out.println("子线程："+semaphore.availablePermits());
             } catch (InterruptedException e) {
 
             }
         }).start();
-        System.out.println("主线程一："+semaphoreA.availablePermits());
-        semaphoreA.release();
-        System.out.println("主线程二："+semaphoreA.availablePermits());
-        semaphoreA.acquire();
-        System.out.println("主线程三："+semaphoreA.availablePermits());
-        semaphoreA.acquire();
-        System.out.println("主线程四："+semaphoreA.availablePermits());
-        semaphoreA.acquire();
-        System.out.println("主线程五："+semaphoreA.availablePermits());
+        System.out.println("主线程一："+semaphore.availablePermits());
+        semaphore.release();
+        System.out.println("主线程二："+semaphore.availablePermits());
+        semaphore.acquire();
+        System.out.println("主线程三："+semaphore.availablePermits());
+        semaphore.acquire();
+        System.out.println("主线程四："+semaphore.availablePermits());
+        semaphore.acquire();
+        System.out.println("主线程五："+semaphore.availablePermits());
     }
 }
