2a189e89385956a0e4357193873e01958e8d5bf3
zhongnanzhanyaodao <262548405@qq.com>
2018-04-12 15:24:01 +0800

    kn

--- src/test/java/knowledge/thread/CountDownLatchExample.java
+++ src/test/java/knowledge/thread/CountDownLatchExample.java
@@ -9,60 +9,31 @@ public class CountDownLatchExample {
      */
     private final static CountDownLatch mCountDownLatch = new CountDownLatch(2);
 
-    /**
-     * 工作线程类
-     */
-    private static class WorkingThread extends Thread {
-        private final int mSleepTime;
-
-        public WorkingThread(String threadName, int sleepTime) {
-            super.setName(threadName);
-            mSleepTime = sleepTime;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("[" + Thread.currentThread().getName() + "] started!");
-            try {
-                Thread.sleep(mSleepTime);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            mCountDownLatch.countDown();
-            System.out.println("[" + Thread.currentThread().getName() + "] end!");
-        }
-    }
-
-    /**
-     * 需要等待的线程类
-     */
-    private static class NeedWaitThread extends Thread {
-
-        public NeedWaitThread(String threadName) {
-            super.setName(threadName);
-        }
-
-        @Override
-        public void run() {
-            System.out.println("[" + Thread.currentThread().getName() + "] started!");
-            try {
-                // 会阻塞在这里等待 mCountDownLatch 里的计数变为0；
-                // 也就是等待另外的WorkingThread调用countDown()
-                mCountDownLatch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("[" + Thread.currentThread().getName() + "] end!");
-        }
-    }
-
     public static void main(String[] args) throws Exception {
-        // 最先run NeedWaitThread
-        new NeedWaitThread("NeedWaitThread").start();
-        // 运行两个工作线程
-        // 工作线程1运行10秒
-        new WorkingThread("WorkingThread1", 10 * 1000).start();
-        // 工作线程2运行5秒
-        new WorkingThread("WorkingThread2", 5 * 1000).start();
+
+        // 运行两个子线程
+        // 子线程1运行10秒
+        new Thread(() -> {
+            try {
+                Thread.sleep(10 * 1000);
+                mCountDownLatch.countDown();
+                System.out.println("子线程1 执行10秒后完成");
+            } catch (InterruptedException e) {
+            }
+        }).start();
+        // 子线程2运行5秒
+        new Thread(() -> {
+            try {
+                Thread.sleep(5 * 1000);
+                mCountDownLatch.countDown();
+                System.out.println("子线程2 执行5秒后完成");
+            } catch (InterruptedException e) {
+            }
+        }).start();
+
+        //主线程等待子线程执行完成
+        mCountDownLatch.await();
+
+        System.out.println("主线程在所有子线程完成后继续向下执行其他...");
     }
 }
