c24d34526364e8afdfc7c8f0f3bddec9aa82aea5
zhongnanzhanyaodao <262548405@qq.com>
2018-04-12 17:46:11 +0800

    kn

--- src/test/java/knowledge/thread/SynchronizedExample.java
+++ src/test/java/knowledge/thread/SynchronizedExample.java
@@ -2,4 +2,26 @@ package knowledge.thread;
 
 
 public class SynchronizedExample {
+
+    private static Object object = new Object();
+
+    public static void methodA(String name) throws InterruptedException {
+        System.out.println(name + "等待执行methodA");
+        Thread.sleep(1 * 1000);
+        synchronized (object) {
+            System.out.println(name + "开始执行methodA");
+            Thread.sleep(10 * 1000);
+            System.out.println(name + "执行methodA完成");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            try {
+                methodA("子线程");
+            } catch (InterruptedException e) {
+            }
+        }).start();
+        methodA("主线程");
+    }
 }
