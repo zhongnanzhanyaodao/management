ea8b47372d04b831e114c78deef50fac1fc06261
zhongnanzhanyaodao <262548405@qq.com>
2018-04-12 15:40:45 +0800

    kn

--- src/test/java/knowledge/thread/CyclicBarrierExample.java
+++ src/test/java/knowledge/thread/CyclicBarrierExample.java
@@ -2,72 +2,38 @@ package knowledge.thread;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-/**
- * 有一个需求：有几个同学约好一起去食堂吃饭，各自都从各自的宿舍出发，然后到宿舍楼下集合。当所有的人都到了宿舍楼下之后，再一起从宿舍楼下出发前往食堂吃饭。
- */
 public class CyclicBarrierExample {
 
-    private static final int THREAD_NUMBER = 3;
-    private static CyclicBarrier sCyclicBarrier = new CyclicBarrier(
-            THREAD_NUMBER, new Runnable() {
-        @Override
-        public void run() {
-            System.out.println("大家都到达了宿舍楼下，一起出发前往食堂吃饭吧。。。");
-        }
-    });
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
 
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors
-                .newFixedThreadPool(THREAD_NUMBER);
-        for (int i = 0; i < THREAD_NUMBER; i++) {
-            executorService.execute(new WalkFromDomitoryToCanteenRunnable(
-                    sCyclicBarrier, "同学" + i));
-        }
-        try {
-            Thread.sleep(10000);//主线程睡眠
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("CyclicBarrier重用");
-        for (int i = THREAD_NUMBER; i < THREAD_NUMBER * 2; i++) {
-            executorService.execute(new WalkFromDomitoryToCanteenRunnable(
-                    sCyclicBarrier, "同学" + i));
-        }
-    }
-
-    /**
-     * 从宿舍到食堂线程
-     *
-     * @author LiuYi
-     */
-    public static class WalkFromDomitoryToCanteenRunnable implements Runnable {
-        private CyclicBarrier mCyclicBarrier;
-        private String mName;
-
-        public WalkFromDomitoryToCanteenRunnable(CyclicBarrier cyclicBarrier,
-                                                 String name) {
-            this.mCyclicBarrier = cyclicBarrier;
-            this.mName = name;
-        }
-
-        @Override
-        public void run() {
-            System.out.println(mName + "开始从宿舍出发。。。");
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+        //子线程1
+        new Thread(() -> {
             try {
-                Thread.sleep(1000);
-                mCyclicBarrier.await();// 等待别同学
-                // 前往食堂
-                System.out.println(mName + "开始从宿舍楼下出发去食堂。。。");
-                Thread.sleep(1000);
-                System.out.println(mName + "达到食堂。。。");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (BrokenBarrierException e) {
-                e.printStackTrace();
+                Thread.sleep(3 * 1000);
+                System.out.println("子线程1 已准备好");
+                cyclicBarrier.await();// 等待其它线程
+                System.out.println("子线程1 开始处理");
+            } catch (Exception e) {
             }
-        }
+        }).start();
+        //子线程2
+        new Thread(() -> {
+            try {
+                Thread.sleep(10 * 1000);
+                System.out.println("子线程2 已准备好");
+                cyclicBarrier.await();// 等待其它线程
+                System.out.println("子线程2 开始处理");
+            } catch (Exception e) {
+            }
+        }).start();
+
+        Thread.sleep(5 * 1000);
+        System.out.println("主线程 已准备好");
+        cyclicBarrier.await();// 等待其它线程
+        System.out.println("主线程 开始处理");
     }
+
+
 }
