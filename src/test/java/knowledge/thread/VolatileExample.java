4450499384e08f2a18efdb9c25eae7cfc871e138
zhongnanzhanyaodao <262548405@qq.com>
2018-04-12 18:10:49 +0800

    kn

--- src/test/java/knowledge/thread/VolatileExample.java
+++ src/test/java/knowledge/thread/VolatileExample.java
@@ -2,4 +2,20 @@ package knowledge.thread;
 
 
 public class VolatileExample {
+
+    private static volatile boolean isRunning;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            while (!isRunning) {
+                System.out.println("子线程 is running");
+            }
+        }).start();
+
+        try {
+            Thread.sleep(2 * 1000);
+        } catch (InterruptedException e) {
+        }
+        isRunning = true;
+    }
 }
