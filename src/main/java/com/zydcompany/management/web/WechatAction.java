e5dce5794fe92d501ccccd27809d8e2846e3aeef
zhongnanzhanyaodao <262548405@qq.com>
2018-06-07 15:34:15 +0800

    wechat token验证

--- src/main/java/com/zydcompany/management/web/WechatAction.java
+++ src/main/java/com/zydcompany/management/web/WechatAction.java
@@ -40,7 +40,7 @@ public class WechatAction {
 
       /*
         加密/校验流程如下:
-        1. 将token、timestamp、nonce三个参数进行字典排序
+        1. 将token、timestamp、nonce三个参数进行字典序排序
         2. 将三个参数字符串拼接成一个字符串进行sha1加密
         3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
       */
