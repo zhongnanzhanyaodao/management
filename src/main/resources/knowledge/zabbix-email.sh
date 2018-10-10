d1d7cd27a2c3e934e835c5d3fa2ebe87677c2d33
duanwaijun <duanwaijun@szunicom.net>
2018-10-10 13:01:29 +0800

    zabbix

--- src/main/resources/knowledge/zabbix-email.sh
+++ src/main/resources/knowledge/zabbix-email.sh
@@ -3,6 +3,6 @@ email_File=/tmp/email.log
 function main(){
         echo "$3" >$email_File
         /usr/bin/dos2unix $email_File
-        /bin/mail -s "$2" "$1" <$email_File
+        /usr/bin/mail -s "$2" "$1" <$email_File
 }
 main "$1" "$2" "$3"
